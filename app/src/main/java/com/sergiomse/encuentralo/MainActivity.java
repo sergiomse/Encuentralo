4c51d16584c9480dde7c1e51935c68206a0f4fdb
sergiomse <sergiomse@gmail.com>
2015-09-23 17:41:02 +0200

    Añadida estructura del MainActivity

--- app/src/main/java/com/sergiomse/encuentralo/MainActivity.java
+++ app/src/main/java/com/sergiomse/encuentralo/MainActivity.java
@@ -2,15 +2,59 @@ package com.sergiomse.encuentralo;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.DisplayMetrics;
+import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
+import android.view.ViewGroup;
+import android.view.ViewTreeObserver;
+import android.widget.FrameLayout;
+import android.widget.LinearLayout;
+import android.widget.ScrollView;
 
 public class MainActivity extends AppCompatActivity {
+
+    private static final String TAG = MainActivity.class.getSimpleName();
+
+    private ScrollView scrollView;
+    private LinearLayout buttonsLayout;
+    private LinearLayout listLayout;
+
+    private FrameLayout.LayoutParams buttonsLayoutParams;
+
+    private DisplayMetrics dm;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        dm = getResources().getDisplayMetrics();
+
+        buttonsLayoutParams = new FrameLayout.LayoutParams(ViewGroup.LayoutParams.MATCH_PARENT, (int) (150 * dm.density));
+
+        scrollView      = (ScrollView) findViewById(R.id.scrollView);
+        buttonsLayout   = (LinearLayout) findViewById(R.id.buttonsLayout);
+        listLayout      = (LinearLayout) findViewById(R.id.listLayout);
+
+        scrollView.getViewTreeObserver().addOnScrollChangedListener(new ViewTreeObserver.OnScrollChangedListener() {
+
+            @Override
+            public void onScrollChanged() {
+                int scrollY = scrollView.getScrollY();
+                Log.d(TAG, "Scroll Y = " + scrollY);
+
+                if(scrollY > 0 && scrollY < 102 * dm.density) {
+                    buttonsLayoutParams.height = (int) (150 * dm.density - scrollY);
+                } else if(scrollY > 102 * dm.density) {
+                    buttonsLayoutParams.height = (int) (48 * dm.density);
+                } else if(scrollY == 0) {
+                    buttonsLayoutParams.height = (int) (150 * dm.density);
+                }
+                buttonsLayout.setLayoutParams(buttonsLayoutParams);
+            }
+        });
     }
 
     @Override
