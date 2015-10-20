13058788969c4307983b2c9609cd2a82f750f3a3
sergiomse <sergiomse@gmail.com>
2015-10-20 22:23:52 +0200

    Eliminado el estado Editar de la toma de fotos

--- app/src/main/java/com/sergiomse/encuentralo/PhotoLocationActivity.java
+++ app/src/main/java/com/sergiomse/encuentralo/PhotoLocationActivity.java
@@ -35,8 +35,7 @@ public class PhotoLocationActivity extends AppCompatActivity {
     private static final String TAG = PhotoLocationActivity.class.getSimpleName();
 
     public static final int VIEW_STATE = 0;
-    public static final int EDIT_STATE = 1;
-    public static final int NEW_STATE = 2;
+    public static final int NEW_STATE = 1;
 
     private int state;
     private long thingId;
@@ -45,7 +44,6 @@ public class PhotoLocationActivity extends AppCompatActivity {
     private EditText etTags;
     private EditText etLocation;
     private Button btnDelete;
-    private Button btnEdit;
     private Button btnCancel;
     private Button btnSave;
 
@@ -60,17 +58,16 @@ public class PhotoLocationActivity extends AppCompatActivity {
         etTags              = (EditText) findViewById(R.id.etTags);
         etLocation          = (EditText) findViewById(R.id.etLocation);
         btnDelete           = (Button) findViewById(R.id.btnDelete);
-        btnEdit             = (Button) findViewById(R.id.btnEdit);
         btnCancel           = (Button) findViewById(R.id.btnCancel);
         btnSave             = (Button) findViewById(R.id.btnSave);
 
 
         Intent intent = getIntent();
         state = intent.getIntExtra("state", 0);
-        setState();
         switch (state) {
             case NEW_STATE:
                 photoFile = new File( (String) intent.getExtras().get("photoFile"));
+                btnDelete.setVisibility(View.GONE);
                 break;
             case VIEW_STATE:
                 thingId = intent.getLongExtra("id", -1);
@@ -172,44 +169,10 @@ public class PhotoLocationActivity extends AppCompatActivity {
         };
     }
 
-    private void setState() {
-//        if(state == VIEW_STATE) {
-//            etTags.setEnabled(false);
-//            etLocation.setEnabled(false);
-//        } else if(state == EDIT_STATE) {
-//            etTags.setEnabled(true);
-//            etLocation.setEnabled(true);
-//        }
-//        displayButtonsByState();
-    }
-
-    private void displayButtonsByState() {
-        switch (state) {
-            case VIEW_STATE:
-                btnDelete.setVisibility(View.VISIBLE);
-                btnEdit.setVisibility(View.VISIBLE);
-                btnCancel.setVisibility(View.GONE);
-                btnSave.setVisibility(View.GONE);
-                break;
-            case EDIT_STATE:
-                btnDelete.setVisibility(View.VISIBLE);
-                btnEdit.setVisibility(View.GONE);
-                btnCancel.setVisibility(View.VISIBLE);
-                btnSave.setVisibility(View.VISIBLE);
-                break;
-            case NEW_STATE:
-                btnDelete.setVisibility(View.GONE);
-                btnEdit.setVisibility(View.GONE);
-                btnCancel.setVisibility(View.VISIBLE);
-                btnSave.setVisibility(View.VISIBLE);
-                break;
-        }
-    }
-
     @Override
     public void onBackPressed() {
         switch (state) {
-            case VIEW_STATE | EDIT_STATE:
+            case VIEW_STATE:
                 super.onBackPressed();
                 break;
             case NEW_STATE:
@@ -218,30 +181,6 @@ public class PhotoLocationActivity extends AppCompatActivity {
                 break;
         }
     }
-
-//    private void setPic() {
-//        // Get the dimensions of the View
-//        int targetW = scrollWrapLayout.getWidth();
-//        int targetH = 500;
-//
-//        // Get the dimensions of the bitmap
-//        BitmapFactory.Options bmOptions = new BitmapFactory.Options();
-//        bmOptions.inJustDecodeBounds = true;
-//        BitmapFactory.decodeFile(photoFile.getAbsolutePath(), bmOptions);
-//        int photoW = bmOptions.outWidth;
-//        int photoH = bmOptions.outHeight;
-//
-//        // Determine how much to scale down the image
-//        int scaleFactor = Math.min(photoW/targetW, photoH/targetH);
-//
-//        // Decode the image file into a Bitmap sized to fill the View
-//        bmOptions.inJustDecodeBounds = false;
-//        bmOptions.inSampleSize = scaleFactor;
-//        bmOptions.inPurgeable = true;
-//
-//        Bitmap bitmap = BitmapFactory.decodeFile(photoFile.getAbsolutePath(), bmOptions);
-//        ivPhoto.setImageBitmap(bitmap);
-//    }
 
     public void buttonDeleteClick(View view) {
         AlertDialog.Builder builder = new AlertDialog.Builder(this);
@@ -266,10 +205,6 @@ public class PhotoLocationActivity extends AppCompatActivity {
         dialog.show();
     }
 
-    public void buttonEditClick(View view) {
-        state = EDIT_STATE;
-        setState();
-    }
 
     public void buttonCancelClick(View view) {
         if(state == NEW_STATE) {
